eead0d28a45ed6a23f9235d8614381b42597b743
HamletShu <ahngo13@naver.com>
2020-04-24 17:50:23 +0900

    add mongoose

---
+++ schemas/board.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const {
+  Types: { ObjectId },
+} = Schema;
+const suggestSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model("Board", suggestSchema);
