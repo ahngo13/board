b8513806c5ad9e8b6c39ee51352ad835e8684f01
HamletShu <ahngo13@naver.com>
2020-04-24 17:30:21 +0900

    routes upload

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/insert", require("./routes/insertRouter"));
+app.use("/update", require("./routes/updateRouter"));
+app.use("/read", require("./routes/readRouter"));
+app.use("/delete", require("./routes/deleteRouter"));
+
 app.listen(3000, ()=>{
     console.log('liston 3000');
 });
